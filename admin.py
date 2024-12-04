d1e4d4b076ae6bdfda0d67503533f00fbaddf760
UniqueSri <139159389+UniqueSri@users.noreply.github.com>
2024-12-04 19:19:51 +0530

    Add files via upload

---
+++ admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import *
+
+admin.site.register(StudentDetails)
+admin.site.register(Marks)
+admin.site.register(Score)
+admin.site.register(PersonalDetails)
